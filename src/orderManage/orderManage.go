7d138d76d04e36fac1f7690e7ee92330eeaa1722
BoningZ <243208074@qq.com>
2023-06-27 19:03:28 +0800

    no encrypt

--- src/orderManage/orderManage.go
+++ src/orderManage/orderManage.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"crypto/elliptic"
-	"crypto/rand"
-	"encoding/hex"
+	//	"crypto/elliptic"
+	//	"crypto/rand"
+	//	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
-	"github.com/tjfoc/gmsm/sm2"
-	"math/big"
+	//	"github.com/tjfoc/gmsm/sm2"
+	//	"math/big"
 	"strconv"
 )
 
@@ -28,8 +28,9 @@ import (
 //买家评价
 //卖家评价
 type Order struct {
-	OrderID      string  `json:"orderId"`
-	Name         []byte  `json:"name"`
+	OrderID string `json:"orderId"`
+	//	Name         []byte  `json:"name"`
+	Name         string  `json:"name"`
 	Quantity     int     `json:"quantity"`
 	Amount       float64 `json:"amount"`
 	OrderTime    string  `json:"orderTime"`
@@ -54,14 +55,23 @@ func (cc *OrderChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		return cc.createOrder(stub, args)
 	} else if function == "getOrder" {
 		return cc.getOrder(stub, args)
+	} else if function == "updateOrder" {
+		return cc.updateOrderStatus(stub, args)
+	} else if function == "addBuyerReview" {
+		return cc.addBuyerReview(stub, args)
+	} else if function == "addSellerReview" {
+		return cc.addSellerReview(stub, args)
 	} else {
 		return shim.Error("Invalid function name.")
 	}
 }
 
 func (cc *OrderChaincode) createOrder(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 8 {
-		return shim.Error("Expecting 7 arguments: OrderID, Name, Quantity, Amount, OrderTime, BuyerID, SellerID, Pubkey")
+	//if len(args) != 8 {
+	//	return shim.Error("Expecting 8 arguments: OrderID, Name, Quantity, Amount, OrderTime, BuyerID, SellerID, Pubkey")
+	//}
+	if len(args) != 7 {
+		return shim.Error("Expecting 7 arguments: OrderID, Name, Quantity, Amount, OrderTime, BuyerID, SellerID, now have " + strconv.Itoa(len(args)))
 	}
 
 	orderID := args[0]
@@ -77,10 +87,11 @@ func (cc *OrderChaincode) createOrder(stub shim.ChaincodeStubInterface, args []s
 	orderTime := args[4]
 	buyerID := args[5]
 	sellerID := args[6]
-	pubkey := args[7]
+	//pubkey := args[7]
 
 	// 对敏感字段进行加密
-	encryptedName, err := encryptData([]byte(name), pubkey)
+	//encryptedName, err := encryptData([]byte(name), pubkey)
+	encryptedName := name
 	if err != nil {
 		return shim.Error("Failed to encrypt name.")
 	}
@@ -113,12 +124,15 @@ func (cc *OrderChaincode) createOrder(stub shim.ChaincodeStubInterface, args []s
 }
 
 func (cc *OrderChaincode) getOrder(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 2 {
-		return shim.Error("Expecting 2 argument: OrderID, PrivateKey")
+	//if len(args) != 2 {
+	//	return shim.Error("Expecting 2 argument: OrderID, PrivateKey")
+	//}
+	if len(args) != 1 {
+		return shim.Error("Expecting 1 argument: OrderID")
 	}
 
 	orderID := args[0]
-	privateKey := args[1]
+	//privateKey := args[1]
 
 	orderJSON, err := stub.GetState(orderID)
 	if err != nil {
@@ -132,7 +146,8 @@ func (cc *OrderChaincode) getOrder(stub shim.ChaincodeStubInterface, args []stri
 	}
 
 	// 对敏感字段进行解密
-	decryptedName, err := decryptData([]byte(order.Name), privateKey)
+	//decryptedName, err := decryptData([]byte(order.Name), privateKey)
+	decryptedName := order.Name
 	if err != nil {
 		return shim.Error("Failed to decrypt name.")
 	}
@@ -251,46 +266,46 @@ func (cc *OrderChaincode) addSellerReview(stub shim.ChaincodeStubInterface, args
 	return shim.Success(nil)
 }
 
-func encryptData(data []byte, publicKeyStr string) ([]byte, error) {
-	// 解码公钥字符串
-	publicKeyBytes, err := hex.DecodeString(publicKeyStr)
-	if err != nil {
-		return nil, err
-	}
-	// 解析公钥
-	x, y := elliptic.Unmarshal(sm2.P256Sm2(), publicKeyBytes)
-	publicKey := &sm2.PublicKey{
-		Curve: sm2.P256Sm2(),
-		X:     x,
-		Y:     y,
-	}
-	// 加密数据
-	ciphertext, err := sm2.EncryptAsn1(publicKey, data, rand.Reader)
-	if err != nil {
-		return nil, err
-	}
-	return ciphertext, nil
-}
-
-func decryptData(ciphertext []byte, privateKeyStr string) ([]byte, error) {
-	// 解码私钥字符串
-	privateKeyBytes, err := hex.DecodeString(privateKeyStr)
-	if err != nil {
-		return nil, err
-	}
-	// 解析私钥
-	privateKey := new(sm2.PrivateKey)
-	privateKey.Curve = sm2.P256Sm2()
-	privateKey.D = new(big.Int).SetBytes(privateKeyBytes)
-	privateKey.PublicKey.Curve = privateKey.Curve
-	privateKey.PublicKey.X, privateKey.PublicKey.Y = privateKey.Curve.ScalarBaseMult(privateKeyBytes)
-	// 解密数据
-	plaintext, err := privateKey.DecryptAsn1(ciphertext)
-	if err != nil {
-		return nil, err
-	}
-	return plaintext, nil
-}
+//func encryptData(data []byte, publicKeyStr string) ([]byte, error) {
+//	// 解码公钥字符串
+//	publicKeyBytes, err := hex.DecodeString(publicKeyStr)
+//	if err != nil {
+//		return nil, err
+//	}
+//	// 解析公钥
+//	x, y := elliptic.Unmarshal(sm2.P256Sm2(), publicKeyBytes)
+//	publicKey := &sm2.PublicKey{
+//		Curve: sm2.P256Sm2(),
+//		X:     x,
+//		Y:     y,
+//	}
+//	// 加密数据
+//	ciphertext, err := sm2.EncryptAsn1(publicKey, data, rand.Reader)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return ciphertext, nil
+//}
+//
+//func decryptData(ciphertext []byte, privateKeyStr string) ([]byte, error) {
+//	// 解码私钥字符串
+//	privateKeyBytes, err := hex.DecodeString(privateKeyStr)
+//	if err != nil {
+//		return nil, err
+//	}
+//	// 解析私钥
+//	privateKey := new(sm2.PrivateKey)
+//	privateKey.Curve = sm2.P256Sm2()
+//	privateKey.D = new(big.Int).SetBytes(privateKeyBytes)
+//	privateKey.PublicKey.Curve = privateKey.Curve
+//	privateKey.PublicKey.X, privateKey.PublicKey.Y = privateKey.Curve.ScalarBaseMult(privateKeyBytes)
+//	// 解密数据
+//	plaintext, err := privateKey.DecryptAsn1(ciphertext)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return plaintext, nil
+//}
 
 func main() {
 	err := shim.Start(new(OrderChaincode))
